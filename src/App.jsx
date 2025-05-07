12a1e36e5b02230ccc6cfde93ebeb2da4c3b05b9
Kidrah <Apple@MacBook-Pro-3.local>
2025-05-07 12:14:07 +0530

    adding fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Dashboard/Dashboard';
 import Transactions from './components/Transactions/Transactions';
 import Budget from './components/Budget/Budget';
-// import other pages as needed
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/transactions" element={<Transactions />} />
         <Route path="/budget" element={<Budget />} />
-        {/* Add routes for other pages here */}
+
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
